0939f4f36be6e63688ef4d0bcbbd96e5dde24ba6
striverz <striverzarc@gmail.com>
2025-10-30 11:26:22 +0530

    update:updated the learing of routing in go

--- 25BuildApi/main.go
+++ 25BuildApi/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 //Course Model
 type Course struct{
@@ -26,4 +33,22 @@ func (c *Course) IsEmpty() bool{
 
 func main(){
 	fmt.Println("Building API In Go")
+
+	r:=mux.NewRouter()
+
+	r.HandleFunc("/",serveHome).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":6969",r))
+}
+
+func serveHome(w http.ResponseWriter,r *http.Request){
+	w.Write([]byte("<h1>Created My First Go Lang Server... At PORT: 6969</h1>"))
+
+}
+
+
+func getAllCourses(w http.ResponseWriter,r *http.Request){
+	fmt.Println("Get All Courses API")
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(courses)
 }
